fc413f162e88de41e70871d9f51550b23e9ffd14
lukepos <70658833+lukepos@users.noreply.github.com>
2026-09-18 22:21:57 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    der Seite, hier wird nur die App selbst zwischengespeichert.
    Bei Änderungen an index.html die VERSION hochzählen.            */
 
-var VERSION = "hhdt-v11";
+var VERSION = "hhdt-v12";
 var CORE = [
   "./",
   "./index.html",
